e97f5c81eff497e73c322708be38256c03f18397
Amanda Gorfine <amandagorfine@Amandas-MacBook-Pro-2.local>
2018-10-17 23:49:43 -0400

    css styling to homepage

--- project2/src/components/Home.js
+++ project2/src/components/Home.js
@@ -11,10 +11,10 @@ class Home extends Component {
 				<h1 className="title">Disney World Trip Adviser</h1>
                 <p className = "intro">Help create a magical trip for you and your family! View all of the rides and restaurants in Disney World, and make your trip happen with ease.</p>
                 <div className ='routeButton'>
-	                <button className = 'mkButton'><Link to="/MagicKingdom" className='mkLink'> Magic Kingdom</Link></button>
-	                <button className = 'epcotButton'><Link to="/Epcot" className='epcotLink'> Epcot</Link></button>
-	                <button className = 'akButton'><Link to="/AnimalKingdom" className='akLink'> Animal Kingdom</Link></button>
-	                <button className = 'hsButton'><Link to="/HollywoodStudios" className='hsLink'> Hollywood Studios</Link></button>
+	                <div className = 'mkButton'><Link to="/MagicKingdom" className='mkLink'> Magic Kingdom</Link></div>
+	                <div className = 'epcotButton'><Link to="/Epcot" className='epcotLink'> Epcot</Link></div>
+	                <div className = 'akButton'><Link to="/AnimalKingdom" className='akLink'> Animal Kingdom</Link></div>
+	                <div className = 'hsButton'><Link to="/HollywoodStudios" className='hsLink'> Hollywood Studios</Link></div>
      			</div>
 			</div>	
 		)
