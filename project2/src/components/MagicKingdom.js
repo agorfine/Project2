60644d71c0814ec7dcb9a2ba8cea6b91937a277a
Amanda Gorfine <amandagorfine@Amandas-MacBook-Pro-2.local>
2018-10-22 08:08:37 -0400

    readme updated

--- project2/src/components/MagicKingdom.js
+++ project2/src/components/MagicKingdom.js
@@ -15,11 +15,21 @@ class MagicKingdom extends Component {
 		// console.log(rides)
 
 		const rides =land.lands.map((land, i) => {
-			return land.attractions ? <RidesRow land = {land} key = {i} onFaveToggle={() => this.props.onFaveToggle()} isFave = {() => this.props.faves.includes()} fave={this.props.faves}/> : '';
+			return land.attractions ? <RidesRow 
+										land = {land} 
+										key = {i} 
+										onFaveToggle={() => this.props.onFaveToggle()} 
+										isFave = {() => this.props.faves.includes()} 
+										fave={this.props.faves}
+										/> : '';
 		})
 
 		const restaurants =land.lands.map((land, i) => {
-			return land.dining ? <RestaurantRow land = {land} key ={i} onFaveToggle={() => this.props.onFaveToggle(land)}/> : '';
+			return land.dining ? <RestaurantRow 
+									land = {land} 
+									key ={i} 
+									onFaveToggle={() => this.props.onFaveToggle(land)}
+									/> : '';
 
 		})
 
